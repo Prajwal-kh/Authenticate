5e3890f3f53214ad5a389acc4b1b8d5eea3d849f
prajwal kharode <Prajwal-kh>
2022-04-07 08:18:21 +0530

    Added Hash & salt authentication using passport.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-    // To encrept user's password we saved in our database install mongoose-encryption first
 // const encrypt = require("mongoose-encryption");
-    // To encrypt using environment var, add it on very top of application to make more secure, also create a .env file in root dir of project:
-    // create a .env file & .gitignore file with node template
-
-const md5 = require("md5");
+// const md5 = require("md5");  //using md5 hashing function
+// const bCrypt = require("bcrypt")
+// const saltRounds = 10;
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express();
 
@@ -18,7 +19,17 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
+// we start implementing passport.js authentication by session package:
+app.use(session({
+    secret:"This is secret string.",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());    //to handle the session using passport
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true})    // & run mongod cmd on new terminal
+// mongoose.set("useCreateIndex",true); // No need to write this now deprecated from ES6 version
 
 // Need to create mongoose.Schema to encrypt, instead of just an object:
 const userSchema = new mongoose.Schema({
@@ -26,14 +37,24 @@ const userSchema = new mongoose.Schema({
     Password: String
 });
 
-    // Encryption method we are using is Env variables:      (Note:- add encryption before creating model)
+// Encryption method we are using is Env variables:      (Note:- add encryption before creating model)
 // userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields:["Password"]});
-    // Delete old database as we created encryption on it.
 
-    // Encrytion method we are using now is Hashing passwords:  (require & install md5 package cast password in Register route & decrypt it in login route)
-
+// To authenticate user on schema, use passport.js for authentication we need to plugin userSchema with passport-local-mongoose:
+userSchema.plugin(passportLocalMongoose);   // To hash & salt our password & to save users into database
 
 const User = new mongoose.model("User", userSchema);
+
+// we createStrategy after we require & install passport packages, start & initialize session & plugin (order is imp)
+passport.use(User.createStrategy());   //local login strategy using passport-local-mongoose
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 app.get("/", function(req,res){
     res.render("home")
@@ -47,38 +68,83 @@ app.get("/register",(req,res)=>{
     res.render("register")
 })
 
+// we need to create secrets routes as user is able to access them while they are logged in:
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    }else{
+        res.redirect("/login")  // if user close browser session cookie gets deleted & user go to the login route again
+    }
+})
+
 app.post("/register", (req,res)=>{
-    const newUser = new User({
-        Username:req.body.username,
-        Password:md5(req.body.password)
-    });
-    // Save will encrypt the file & find will decrypt it.
-    newUser.save((err)=>{
+   /* //bCrypt hashing method:   (we need to again delete DB with old username & password to store new Hashing authentication)
+    bCrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // hash will contains hash code for user typed password:
+        const newUser = new User({
+            Username:req.body.username,
+            // Password:md5(req.body.password)
+            Password:hash
+        });
+        // Save will encrypt the file & find will decrypt it.
+        newUser.save((err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
+    }); */
+    //refer passport.js docs & passport-local-mongoose npm package docs to write regiter method on Database & login code:
+    User.register({username:req.body.username}, req.body.password, function(err, user){
         if(err){
             console.log(err);
+            res.redirect("/register")
         }else{
-            res.render("secrets");
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets")
+            });
         }
     });
 });
 
 app.post("/login",(req,res)=>{
-    const enteredName = req.body.username;
-    const enteredPassword =md5(req.body.password);
+    /*const enteredName = req.body.username;
+    const enteredPassword =req.body.password;   //earlier we cast it with md5 hashing function
     User.findOne({Username : enteredName},function(err, foundUser){
         if(err){
             console.log(err);
         }else{
             if(foundUser){   
-                if(foundUser.Password===enteredPassword){
-                    res.render("secrets");
-                }
-                
+                // compare user's entered password with database stored password:
+                bCrypt.compare(enteredPassword, foundUser.Password, function(err, result) {
+                    if(result===true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
-    })
+    }) */
+    const user = new User({
+        Username : req.body.username,
+        Password : req.body.password
+    });
+    // Refer passport.js -> Sessions docs
+    req.login(user, function(err) {
+        if (err) { 
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+            res.redirect("/secrets")
+        })
+      }
+    });
 });
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect('/');
+})
 
 app.listen(3000,()=>{
     console.log('connected to 3000');
-})
+});
